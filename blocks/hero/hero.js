2509661a3a9f509c42f8a95bea8dc0be3c0425bd
smit06 <30765791+smit06@users.noreply.github.com>
2023-10-05 20:58:01 +0530

    Update hero.js

---
+++ blocks/hero/hero.js
@@ -0,0 +1,36 @@
+function headerShow () {
+    var getHeader = document.querySelector('.header > div > div');
+    run().then( function(response){
+        var html = document.createElement("html");
+        html.innerHTML = response;
+        var body = html.querySelector(".aem-Grid").children;
+        getHeader.innerHTML = body;
+    })
+
+}
+
+
+
+function run() {
+ 
+    return new Promise( (resolve, reject) => {
+        // Creating Our XMLHttpRequest object 
+        let xhr = new XMLHttpRequest();
+     
+        // Making our connection  
+        let url = 'https://qa.tataaia.com/content/experience-fragments/tataaia_life_insuran/en/aem_demo/xfheader/master.html';
+        xhr.open("GET", url, true);
+     
+        // function execute after request is successful 
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                resolve(this.responseText);
+            }
+        }
+        // Sending our request 
+        xhr.send();
+    })
+}
+
+headerShow();
+
