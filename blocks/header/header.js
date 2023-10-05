5d5d10e4fd9d5363480d65cd838aa08bf38155ee
smit06 <30765791+smit06@users.noreply.github.com>
2023-10-05 21:19:04 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -3,6 +3,44 @@ import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
+
+export default function headerShow(block) {
+    run().then( function(response){
+        var quote = block.querySelector(":scope > div > div");
+        var html = document.createElement("html");
+        html.innerHTML = response;
+        var body = html.querySelector(".aem-Grid").children;
+        var getHeader = quote.closet('header > div > div');
+        getHeader.innerHTML = body;
+    })
+
+}
+
+
+
+function run() {
+ 
+    return new Promise( (resolve, reject) => {
+        // Creating Our XMLHttpRequest object 
+        let xhr = new XMLHttpRequest();
+     
+        // Making our connection  
+        let url = 'https://qa.tataaia.com/content/experience-fragments/tataaia_life_insuran/en/aem_demo/xfheader/master.html';
+        xhr.open("GET", url, true);
+     
+        // function execute after request is successful 
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                resolve(this.responseText);
+            }
+        }
+        // Sending our request 
+        xhr.send();
+    })
+}
+
+headerShow();
+
 function closeOnEscape(e) {
   if (e.code === 'Escape') {
     const nav = document.getElementById('nav');
